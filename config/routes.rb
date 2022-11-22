c4aba94dab9f423605a29697fb47f1c4ea414f30
Ramazan Bulut <ramazan.bulut@ISTPRSMSL27751.local>
2022-11-22 13:29:45 +0300

    2 page with navbar

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get "/about-us", to: "about#index", as: :about
+
+  get "/", to: "main#index"
+   
+  
+end
