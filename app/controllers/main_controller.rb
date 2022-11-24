5738d50902ac3612d7355cc60f88942d3f962d21
Ramazan Bulut <ramazan.bulut@ISTPRSMSL27751.home>
2022-11-24 10:17:19 +0300

    user model

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
     def index
+        flash[:notice]= "Logged in successfully"
     end
 
 end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+#email:string
+# password_digest:string
+
+# password:string virtual
+# password_confirmation: string virtual
+
+
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "must be a vail email address"}
+
+end
